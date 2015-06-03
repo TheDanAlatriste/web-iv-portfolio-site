a91ae1f9521ba0645e655aee303c21a278894518
TheDanAlatriste <2e:c2:d8:38:5d:54:1c:48:be:57:e3:dd:08:4d:8f:3d>
2015-06-03 12:18:26 -0500

    Validation and Formatter
    Validated the code and ran it through a formatter.

--- contact-thanks.php
+++ contact-thanks.php
@@ -1,25 +1,19 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
-
 <head>
-	<title>ALATRISTE</title>
-    <link rel="stylesheet" href="css/foundation.css"/>
-    <meta http-equiv="refresh" content="5,url=index.html"/>
-	<link rel="stylesheet" type="text/css" href="css/contact-form.css"/>
-    <script src="//use.typekit.net/lxn8pgc.js"></script>
-	<script>try{Typekit.load();}catch(e){}</script>
+  <title>ALATRISTE</title>
+  <link rel="stylesheet" href="css/foundation.css"/>
+  <meta http-equiv="refresh" content="5,url=index.html"/>
+  <link rel="stylesheet" type="text/css" href="css/contact-form.css"/>
+  <script src="//use.typekit.net/lxn8pgc.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
 </head>
-
 <body>
-	
-    <section class="large-6 large-offset-3 medium-8 medium-offset-2 small-12">
-		<h1>Thanks!</h1>
-        <p>Your message has been sent!</p>
-        <div class="loader large-2 medium-2">Loading...</div>
-        <p id="redirecting">Redirecting</p>
-		
-		<p id="option">If not, click <a href="index.html">here</a> to go back.</p>
-    <section>
-
+  <section class="large-6 large-offset-3 medium-8 medium-offset-2 small-12">
+  <h1>Thanks!</h1>
+  <p>Your message has been sent!</p>
+  <div class="loader large-2 medium-2">Loading...</div>
+  <p id="redirecting">Redirecting</p>
+  <p id="option">If not, click <a href="index.html">here</a> to go back.</p>
+  <section>
 </body>
-
 </html>
